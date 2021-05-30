72daaf8ee0c8ac68da19018cdffc191746c80a04
masahito-nagayama <me18sf67m@gmail.com>
2021-05-30 12:04:38 +0900

    add components

---
+++ components/Music.vue
@@ -0,0 +1,65 @@
+<template>
+  <main class="main">
+    <button
+      class="musicBtn"
+      @click="musicStart"
+    >
+      Music
+    </button>
+    <button
+      class="startBtn"
+      @click="appStart"
+      v-if="isStarted==false">
+      Start
+    </button>
+    <secction
+      class="touchZone"
+      v-else>
+      &nbsp;
+    </secction>
+  </main>
+</template>
+
+<script>
+import Tone from 'tone'
+
+export default {
+  data(){
+    return {
+      isStarted: false
+    }
+  },
+  methods:{
+    appStart(){
+      this.isStarted = true
+    },
+    musicStart() {
+      var synth = new Tone.Synth().toMaster()
+      synth.triggerAttackRelease('C4', '8n')
+    }
+  }
+}
+</script>
+
+<style>
+.main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.startBtn,
+.musicBtn {
+  padding: 10px 20px;
+}
+
+.touchZone {
+  background: linear-gradient(45deg, rgb(255,255,255),rgb(100,255,255));
+  width: 100%;
+  min-width: 100%;
+  height: 100vh;
+  min-height: 100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,15 @@
 <template>
-  <main class="main">
-    <button
-      class="startBtn"
-      @click="appStart"
-      v-if="isStarted==false">
-      Start
-    </button>
-    <secction
-      class="touchZone"
-      v-else>
-      &nbsp;
-    </secction>
-  </main>
+  <no-ssr>
+    <Music />
+  </no-ssr>
 </template>
 
 <script>
+import Music from '~/components/Music'
+
 export default {
-  data(){
-    return {
-      isStarted: false
-    }
-  },
-  methods:{
-    appStart(){
-      this.isStarted = true
-    }
-  }
+  components: {
+    Music
+  }  
 }
 </script>
-
-<style>
-.main {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.startBtn {
-  padding: 10px 20px;
-}
-
-.touchZone {
-  background: linear-gradient(45deg, rgb(255,255,255),rgb(100,255,255));
-  width: 100%;
-  min-width: 100%;
-  height: 100vh;
-  min-height: 100vh;
-}
-</style>
